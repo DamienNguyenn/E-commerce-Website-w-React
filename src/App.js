e070df163792e36008933d3965ddf162774efba0
Damien <nguyentrihieu253@gmail.com>
2022-09-13 20:32:16 +0200

    Add Cart

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -5,8 +6,35 @@ import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from "./data";
 import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  // function addToCart(book) {
+  //   const dupeItem = cart.find((item) => +item.id === +book.id);
+  //   if (dupeItem) {
+  //     setCart(
+  //       cart.map((item) => {
+  //         if (item.id === dupeItem.id) {
+  //           return {
+  //             ...item,
+  //             quantity: item.quantity + 1,
+  //           };
+  //         } else {
+  //           return item;
+  //         }
+  //       })
+  //     );
+  //   } else {
+  //     setCart([...cart, { ...book, quantity: 1 }]);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   console.log(cart);
+  // }, [cart]);
+
   return (
     <Router>
       <div className="App">
@@ -14,7 +42,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/books" element={<Books books={books} />} />
-          <Route path="/books/:id" element={<BookInfo books={books} />} />
+          <Route
+            path="/books/:id"
+            element={<BookInfo books={books} addToCart={addToCart} />}
+          />
+          <Route path="/cart" element={<Cart books={books} />} />
         </Routes>
         <Footer />
       </div>
